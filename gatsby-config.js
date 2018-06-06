598742af0937ff54e7cb2c76d5277d809a1bd6a2
wabdsgn <wab2com@gmail.com>
2018-06-07 00:17:34 +0200

    define prod env. & noindex condition
    # Conflicts:
    #	gatsby-config.js
    #	package.json
    #	yarn.lock

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,32 @@
+const {
+  NODE_ENV,
+  // TODO certfier le domaine
+  URL: NETLIFY_SITE_URL = 'http://desgranges.rocks',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env;
+// TODO remplacer 'isNetlifyProduction' pour indexer le site
+// const isNetlifyProduction = NETLIFY_ENV === 'production';
+const isNetlifyProduction = false;
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
 module.exports = {
   siteMetadata: {
+    siteUrl: 'http://desgranges.rocks',
     title: 'Desgranges',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: isNetlifyProduction
+        ? { policy: [{ userAgent: '*' }] }
+        : {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+    },
+  ],
 };
