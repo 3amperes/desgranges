b0bfcba5072c7b403fecac4a539c1a90fbe4ab43
Nicolas Morel <nmr.morel@gmail.com>
2018-06-25 19:54:27 +0200

    Better implementation

---
+++ src/components/layout/AnimatedScreen.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { TimelineLite } from 'gsap';
+import { Box } from 'components';
+import { absolute } from 'utils/mixins';
+import { TransitionGroup, Transition } from 'react-transition-group';
+import withRouter from 'react-router/withRouter';
+
+const animationDuration = 600;
+const tweenDuration = animationDuration / 3000 * 2;
+
+const AnimationComponentWrapper = Box.extend`
+  ${absolute({ top: 0, bottom: 0, left: 0, right: 0 })};
+  z-index: 999;
+  transform: translateX(-100%);
+`;
+
+class AnimationComponent extends React.Component {
+  wrapper = React.createRef();
+
+  animate = () => {
+    const wrapper = this.wrapper.current;
+
+    this.animation = new TimelineLite();
+    this.animation
+      .set(wrapper, { x: '-100%' })
+      .to(wrapper, tweenDuration, { x: '0%' })
+      .to(wrapper, tweenDuration, { x: '-100%' }, `+=${tweenDuration}`)
+      .set(wrapper, { x: '-100%' });
+    this.animation.play();
+  };
+
+  render() {
+    return <AnimationComponentWrapper innerRef={this.wrapper} bg={'sea'} />;
+  }
+}
+
+class ScreenWrapper extends React.Component {
+  shouldComponentUpdate() {
+    // Do not update previous screen in order to keep the content visible while transitionning to new screen
+    return this.props.location.pathname === window.location.pathname;
+  }
+
+  render() {
+    const { state, render } = this.props;
+    return render(
+      state === 'entering'
+        ? {
+            style: {
+              display: 'none',
+            },
+          }
+        : {}
+    );
+  }
+}
+
+class _AnimatedScreen extends React.Component {
+  animationComponent = React.createRef();
+
+  onEntering = () => {
+    this.animationComponent.current.animate();
+  };
+
+  render() {
+    const { render, location } = this.props;
+    return (
+      <React.Fragment>
+        <AnimationComponent ref={this.animationComponent} />
+        <TransitionGroup component={null}>
+          <Transition
+            key={location.pathname}
+            timeout={animationDuration}
+            onEntering={this.onEntering}
+          >
+            {state => (
+              <ScreenWrapper
+                state={state}
+                location={location}
+                render={render}
+              />
+            )}
+          </Transition>
+        </TransitionGroup>
+      </React.Fragment>
+    );
+  }
+}
+
+export const AnimatedScreen = withRouter(_AnimatedScreen);
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import withRouter from 'react-router-dom/withRouter';
-import { TransitionGroup, Transition } from 'react-transition-group';
-import { TimelineLite } from 'gsap';
 import { injectGlobal, ThemeProvider } from 'styled-components';
 import { Toggle } from 'react-powerplug';
 import theme from 'utils/theme';
 import { Header, Footer, Navigation } from 'components';
+import { AnimatedScreen } from 'components/layout/AnimatedScreen';
 import { Box } from 'components';
 import { getFontFace } from 'utils/mixins';
 
@@ -25,64 +23,6 @@ injectGlobal`
   }
 `;
 
-class AnimatedScreen extends React.Component {
-  wrapper = React.createRef();
-
-  animate = () => {
-    const wrapper = this.wrapper.current;
-
-    this.animation = new TimelineLite();
-    this.animation
-      .set(wrapper, { x: '-100%' })
-      .to(wrapper, 0.4, { x: '0%' })
-      .to(wrapper, 0.4, { x: '-100%' }, '+=0.4')
-      .set(wrapper, { x: '-100%' });
-    this.animation.play();
-  };
-
-  render() {
-    return (
-      <Box
-        innerRef={this.wrapper}
-        bg={'sea'}
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          zIndex: 1000,
-          transform: 'translateX(-100%)',
-        }}
-      />
-    );
-  }
-}
-
-class ViewWrapper extends React.Component {
-  shouldComponentUpdate() {
-    // Do not update previous screen in order to keep the content visible while transitionning to new screen
-    return this.props.location.pathname === window.location.pathname;
-  }
-
-  render() {
-    const { state, children } = this.props;
-    return (
-      <Box
-        bg="gray.light"
-        pt={120}
-        style={{
-          position: 'absolute', // absolute is required because 2 screens can be on top of each other during the transition
-          width: '100%',
-          opacity: state === 'entering' ? 0 : 1,
-        }}
-      >
-        {children}
-      </Box>
-    );
-  }
-}
-
 const MainWrapper = Box.extend`
   height: ${props => (props.overflowHidden ? '100vh' : 'auto')};
   overflow-y: ${props => (props.overflowHidden ? 'hidden' : 'visible')};
@@ -96,29 +36,22 @@ class Layout extends React.Component {
   };
 
   render() {
-    const { children, location, data } = this.props;
+    const { children } = this.props;
     return (
       <ThemeProvider theme={theme}>
         <Toggle initial={false}>
           {({ on, toggle, set }) => (
-            <MainWrapper overflowHidden={on} p={0}>
-              <AnimatedScreen ref={this.animatedScreen} />
-              <TransitionGroup component={null}>
-                <Transition
-                  key={location.pathname}
-                  timeout={600}
-                  onEntering={this.onEntering}
-                >
-                  {state => (
-                    <ViewWrapper state={state} location={location}>
-                      <Header onToggleMenu={toggle} isMenuOpened={on} />
-                      <Box pb={6}>{children()}</Box>
-                      <Footer />
-                      <Navigation isOpened={on} onClose={() => set(false)} />
-                    </ViewWrapper>
-                  )}
-                </Transition>
-              </TransitionGroup>
+            <MainWrapper overflowHidden={on} p={0} bg="gray.light" pt={120}>
+              <Header onToggleMenu={toggle} isMenuOpened={on} />
+              <AnimatedScreen
+                render={props => (
+                  <Box pb={6} {...props}>
+                    {children()}
+                  </Box>
+                )}
+              />
+              <Footer />
+              <Navigation isOpened={on} onClose={() => set(false)} />
             </MainWrapper>
           )}
         </Toggle>
@@ -131,7 +64,7 @@ Layout.propTypes = {
   children: PropTypes.func,
 };
 
-export default withRouter(Layout);
+export default Layout;
 
 export const query = graphql`
   query SiteTitleQuery {
